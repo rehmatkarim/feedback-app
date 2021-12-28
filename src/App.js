4e194603b0b3ba2a1fce99d7ce54ed48963970d7
rehmat <karimrehmat942@gmail.com>
2021-12-29 02:42:19 +0500

    routes and about page done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
+import AboutPage from "./pages/AboutPage";
+import AboutIconLink from "./components/AboutIconLink";
 import FeedbackData from "./data/FeedbackData";
 
 function App() {
@@ -15,17 +18,32 @@ function App() {
   };
   const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4();
-    setFeedback([newFeedback,...feedback]);
+    setFeedback([newFeedback, ...feedback]);
   };
   return (
-    <>
+    <Router>
       <Header />
       <div className="container">
-        <FeedbackForm addFeedback={addFeedback} />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedBackItem} />
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <>
+                <FeedbackForm addFeedback={addFeedback} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList
+                  feedback={feedback}
+                  handleDelete={deleteFeedBackItem}
+                />
+              </>
+            }
+          ></Route>
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
+        <AboutIconLink />
       </div>
-    </>
+    </Router>
   );
 }
 
